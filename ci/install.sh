8a0dcdfb639fb363448fac9e0953b7d076a24e6e
Georgiy <@GeorgiyX>
2020-03-11 17:59:13 +0300

    ci: add coverage test

--- ci/install.sh
+++ ci/install.sh
@@ -5,5 +5,7 @@ sudo apt-get install --yes cmake
 sudo apt-get install --yes libgtest-dev
 sudo apt-get install --yes valgrind
 sudo apt-get install --yes cppcheck
+sudo apt-get install --yes curl
+export CODECOV_TOKEN="5c299964-5a65-411d-bbfe-e7ac089395aa" # :)
 sudo cmake /usr/src/gtest/CMakeLists.txt
 (cd /usr/src/gtest && sudo make && sudo cp *.a /usr/lib)
